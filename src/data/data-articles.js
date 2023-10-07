df2527bdd002dab8e4c39972f2def4a1c020561a
Aniket <74449359+Si-ddhartha@users.noreply.github.com>
2023-10-07 18:47:47 +0530

    Added an article

--- src/data/data-articles.js
+++ src/data/data-articles.js
@@ -1,5 +1,11 @@
 export const dataArticles = [
     {
+        title: 'AniGAN: Face Generator',
+        description: "In the article, we delve into the fascinating world of Generative Adversarial Networks (GANs) and their application in generating lifelike faces. This article provides a comprehensive introduction to GANs, explaining how they work, followed by a step-by-step guide where I guide you on building a face generation model using TensorFlow. Discover the cutting-edge techniques behind creating AI-generated faces and unlock the potential of GANs in the ever-evolving realm of artificial intelligence.",
+        link: 'https://medium.com/@aniket1.00111/anigan-exploring-face-generation-through-deep-learning-0dabc3c6b9ff'
+    },
+
+    {
         title: 'Batch Normalization',
         description: "In the article, we delve into a critical aspect of deep learning model design. By comparing the effects of applying batch normalization before and after the activation function, we explore the implications for model convergence, training speed, and overall performance. This comprehensive analysis provides valuable insights for deep learning practitioners, helping them make informed decisions when fine-tuning neural networks to achieve optimal results in various applications.",
         link: 'https://medium.com/@aniket1.00111/optimizing-neural-networks-batch-normalization-before-vs-after-activation-function-a969be6b5cf3'
